7efebbbe8152f541eaf8f611c334ab29420c5f9c
Mohamed-Elashry1 <mohamedelashry2216@gmail.com>
2025-12-08 13:51:40 -0500

    all

--- jobs/views.py
+++ jobs/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from .models import Job
 
 def job_list (request):
-    jobs = Job.objects.all()
+    jobs = Job.objects.all().only('id',  'image', 'title', 'location', 'job_type', 'created_at')
     return render(request, 'jobs.html', {"jobs":jobs})
+
+
+def job_details(request, **kwargs):
+    job = Job.objects.get(id=kwargs['id'])
+    return render(request, 'job_details.html', {"job":job})
